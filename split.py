832294a1fa4f130d764833d5f9b2b0d54febb74f
Zakariya Sattar <zakariyasattar03@gmail.com>
2020-11-20 13:53:45 -0600

    added song data

--- split.py
+++ split.py
@@ -1,3 +1,20 @@
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
-ffmpeg_extract_subclip("audio.mp3", 0, 60, targetname="result/shorter_audio.mp3")
+song_data = [
+    ["00:00", "Mike Noise", "Low Earth Orbit"],
+    03:26 SHAP3S - Swell
+    06:17 Shortwire - Reconfig
+    09:59 DOOMROAR - Sagittarius (Ft. ALISON)
+    13:37 Com Truise - Hyperlips
+    18:18 Emil Rottmayer - Descend
+    22:26 DOOMROAR - The Discovery (Ft. Krosia)
+    26:20 HOME - I See You
+    29:02 Unnamed - Sundown
+    30:59 Emil Rottmayer - K.E.Y.S
+    35:00 HOME - Billiards
+    37:56 Mitch Murder - Mirage
+]
+
+for data in song_data:
+    previous_end_time = 
+    ffmpeg_extract_subclip("cybercity.mp3", convertToMilli(data[0]), 60, targetname = data[2] + ".mp3")
